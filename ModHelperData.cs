0ea532cd9ebc994b2e7f9e49437a9fdf654f9093
Malcolm Roalson <mtroalson@gmail.com>
2025-02-05 17:50:44 -0600

    update modhelperdata, fix tags

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,8 @@ namespace EditPlayerData;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "37.3";
     public const string Version = "1.4";
     public const string Name = "EditPlayerData";
-
-    public const string Description = "A mod that lets you permanently edit your account data.";
-
+    
     public const string RepoOwner = "MaliciousFiles";
-    public const string RepoName = "EditPlayerData";
 }
