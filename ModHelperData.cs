71023307b3295c56f77ee18a09d796f11bebdcfb
Malcolm Roalson <mtroalson@gmail.com>
2025-02-22 20:48:07 -0600

    change version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace EditPlayerData;
 
 public static class ModHelperData
 {
-    public const string Version = "1.4";
+    public const string Version = "1.4.1";
     public const string Name = "EditPlayerData";
     
     public const string RepoOwner = "MaliciousFiles";
